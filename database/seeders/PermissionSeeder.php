80fd9029269ade31649180b61e0a226ed35ca7cb
unknown <ii_amokrane@esi.dz>
2021-10-12 20:13:18 +0200

    changement

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -17,11 +17,12 @@ class PermissionSeeder extends Seeder
         DB::table("permissions")->insert([
             ["nom"=> "ajouter une facture"],
             ["nom"=> "consulter une facture"],
-            
-
+            ["nom"=> "supprimer une facture"],
+            ["nom"=> "editer une facture"],
             ["nom"=> "ajouter un fournisseur"],
             ["nom"=> "consulter une fournisseur"],
             ["nom"=> "editer un fournisseur"],
+            ["nom"=> "supprimer un fournisseur"],
 
             
         ]);
