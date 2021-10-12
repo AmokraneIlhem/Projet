13bc31563ee78241f332e7a94417c67ebc5a3cb6
unknown <ii_amokrane@esi.dz>
2021-10-12 20:18:14 +0200

    changement

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,7 @@ class User extends Authenticatable
         return $this->belongsTo(Role::class); 
     }
     
-    public function permissions(){
-        return $this->belongsToMany(Permission::class);
-    }
+  
 
     public function hasRole( $role){
         return $this->role()->where("name",$role)->first(); 
