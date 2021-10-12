8c960141fcd8d35a4ba5586fd290198fa31540ee
unknown <ii_amokrane@esi.dz>
2021-10-12 20:19:33 +0200

    changement

--- app/Models/Facture.php
+++ app/Models/Facture.php
@@ -8,6 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Facture extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'montant',
+        'fournisseur_id',
+        'structure_id',
+        'dateDebut',
+        'dateFin',
+        'etat'
+        
+    ];
     public function structure(){
         return $this->belongsTo(Structure::class); 
     }
