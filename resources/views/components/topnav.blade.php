8819a599cc15f16074f97d5c6f28de1d4f6c4cc0
unknown <ii_amokrane@esi.dz>
2021-10-12 20:11:43 +0200

    changement

--- resources/views/components/topnav.blade.php
+++ resources/views/components/topnav.blade.php
@@ -15,19 +15,7 @@
       <!-- Messages Dropdown Menu -->
      
       <!-- Notifications Dropdown Menu -->
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-bell"></i>
-          <span class="badge badge-warning navbar-badge">15</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <span class="dropdown-header">15 Notifications</span>
-         
-          
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-        </div>
-      </li>
+    
       <li class="nav-item">
         <a class="nav-link" data-widget="fullscreen" href="#" role="button">
           <i class="fas fa-expand-arrows-alt"></i>
