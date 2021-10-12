fadd53f29fcf178e1e53674eda3f1198652475c5
unknown <ii_amokrane@esi.dz>
2021-10-12 20:18:44 +0200

    changement

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -7,8 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
+    protected $fillable = [
+        'name',
+        
+        
+    ];
     use HasFactory;
     public function users(){
         return $this->hasMany(User::class); 
+    }
+    public function permissions(){
+        return $this->belongsToMany(Permission::class);
+    }
+    public function hasPermission( $permission){
+        return $this->permissions()->where("nom",$permission)->first(); 
     }
 }
